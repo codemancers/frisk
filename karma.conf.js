551f5f936d82ce5e4f795ba0b17293b5c5331804
Emil Soman <emil.soman@gmail.com>
2014-02-12 13:51:28 +0530

    Load AngularJS first in karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'app/assets/javascripts/angular.min.js',
       'app/assets/javascripts/**/*.coffee',
       'spec/javascripts/**/*spec.coffee'
     ],
